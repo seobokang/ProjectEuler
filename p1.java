0e713ccf9932c8eb637f0233877fff8868803b72
Bokang Seo <bokang185@naver.com>
2016-03-07 04:45:30 +0900

    possible compile ver

--- p1.java
+++ p1.java
@@ -1,6 +1,4 @@
-package testt;
-
-public class tt {
+public class p1 {
 	public static void main(String[] args){
 		int total = 0;
 		for(int i = 1; i < 1000 ; i++){
@@ -11,3 +9,4 @@ public class tt {
 		System.out.print(total);
 	}
 }
+
