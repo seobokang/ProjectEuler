6e78fdec9fe814d9b85a5e06d502a5c64cab33ef
Bokang Seo <bokang185@naver.com>
2016-03-07 05:45:45 +0900

    p5 solution

---
+++ p5.java
@@ -0,0 +1,20 @@
+public class p5{
+	
+	public static void main(String[] args){
+		int first = 1;
+		int last = 20;
+		int num = last;
+		int i ;
+		while(true){
+
+			for(i = first ; i <= last ; i++){
+				if(num % i != 0)
+					break;
+                	}
+			if(i == last+1) break;
+			num++;
+		}
+		System.out.println(num);
+
+	}
+}
