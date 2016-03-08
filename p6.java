9c3159183b2b0f9b4134819baff5b08227bb88c0
Bokang Seo <bokang185@naver.com>
2016-03-09 08:47:18 +0900

    p6 sol

---
+++ p6.java
@@ -0,0 +1,24 @@
+public class p6 {
+	public static void main(String[] args){
+			int start = 1;
+			int end = 100;
+			
+			System.out.println(SquareOfSum(start,end)-SumOfSquare(start,end));
+	}
+
+	public static int SquareOfSum(int start,int end){
+		int result = 0;
+		for(int i = start ; i <= end ; i++){
+			result += i;
+		} 
+		return result*result ;
+		
+	}
+	public static int SumOfSquare(int start,int end){
+		int result = 0;
+		for(int i = start ; i <= end ; i++){
+			result += i * i ;
+		}
+		return result;		
+	}
+}
