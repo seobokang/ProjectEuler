715ce4d2b92a19009507a4f541ee51c0214d12a4
Bokang Seo <bokang185@naver.com>
2016-03-25 06:55:14 +0000

    p012 func renaming

--- p012.java
+++ p012.java
@@ -1,21 +1,21 @@
 public class p012{
 	public static void main(String[] args){
-		System.out.println(CaseNumTriangular(Integer.parseInt(args[0])));
+		System.out.println(TriangularOfLeastDivisor(Integer.parseInt(args[0])));
 	}
-	public static int DivisorNum(int num){
+	public static int DivisorCount(int num){
 		int count = 0;
 		for(int i = 1 ; i <= num ; i++){
 			if(num%i == 0) count++;
 		}
 		return count;
 	}
-	public static int CaseNumTriangular(int ListDivisor){
+	public static int TriangularOfLeastDivisor(int LeastDivisor){
 		int dnum = 0 ;
 		int TriangularNum = 0;
 		int max = 0; 
-		for(int i = 1 ; dnum < ListDivisor ; i++){
+		for(int i = 1 ; dnum < LeastDivisor ; i++){
 			TriangularNum += i;
-			dnum = DivisorNum(TriangularNum);
+			dnum = DivisorCount(TriangularNum);
 			if (dnum > max) max = dnum;
 			System.out.printf("%40d%10d%10d\n",TriangularNum,dnum,max);
 		}
