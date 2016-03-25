01674d108ec6852e51c2b7c718fac6b430f1e8cb
Bokang Seo <bokang185@naver.com>
2016-03-25 08:15:27 +0000

    p012 divisor check using exponent data

--- p012.java
+++ p012.java
@@ -1,12 +1,35 @@
+import java.util.ArrayList;
 public class p012{
 	public static void main(String[] args){
 		System.out.println(TriangularOfLeastDivisor(Integer.parseInt(args[0])));
 	}
 	public static int DivisorCount(int num){
-		int count = 0;
-		for(int i = 1 ; i <= num ; i++){
-			if(num%i == 0) count++;
+		int count = 1;
+		int i;
+
+		ArrayList<Integer> BaseData = new ArrayList<>();
+		ArrayList<Integer> ExponentData = new ArrayList<>();
+
+		if (num == 1)
+			return 1;
+
+		while(num != 1){
+			for(i = 2 ; num%i != 0 ; i++);
+			num/=i;
+			int pos = BaseData.indexOf(new Integer(i));
+			if(pos != -1) {
+				ExponentData.set(pos,ExponentData.get(pos)+1);
+			}
+			else {
+				BaseData.add(new Integer(i));
+				ExponentData.add(new Integer(1));
+			}
 		}
+
+		for (Integer j : ExponentData){
+			count *= (j+1);
+		}
+
 		return count;
 	}
 	public static int TriangularOfLeastDivisor(int LeastDivisor){
@@ -17,7 +40,7 @@ public class p012{
 			TriangularNum += i;
 			dnum = DivisorCount(TriangularNum);
 			if (dnum > max) max = dnum;
-			System.out.printf("%40d%10d%10d\n",TriangularNum,dnum,max);
+			System.out.printf("%40d%10d%10d%10d\n",TriangularNum,i,dnum,max);
 		}
 		return TriangularNum;
 	}
