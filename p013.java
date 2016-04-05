f338370761352940e02ab8ac284d9ef3893046d9
Bokang Seo <bokang185@naver.com>
2016-04-05 06:30:38 +0000

    not complete:)

--- p013.java
+++ p013.java
@@ -1,5 +1,16 @@
+import java.io.*;
 public class p013{
-	public static void main(){
-		
+	public static void main(String args[]){
+		String filename = "p013_data.txt";
+		try {
+			BufferedReader in = new BufferedReader(new FileReader(filename));
+			String FileStrLine;
+			while((FileStrLine = in.readLine()) != null){
+				
+			}
+		}catch (IOException e){
+			System.err.println(e);
+			System.exit(1);
+		}
 	}
 }
