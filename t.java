d06ac2a83d821b515c28c6abcb7b69caacb054b6
Bokang Seo <bokang185@naver.com>
2016-04-05 06:31:32 +0000

    test file

---
+++ t.java
@@ -0,0 +1,8 @@
+import java.io.*;
+public class t{
+	public static void main (String args[]){
+		String t = "abcde1234";
+		System.out.println(t.substring(0,3));
+		System.out.println(t.substring(3,5));
+	}
+}
